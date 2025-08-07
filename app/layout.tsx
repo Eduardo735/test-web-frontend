be9f6c34e0192e93f5d6cffee7d69058b2df4b1c
Eduardo735 <gentlelife735@gmail.com>
2025-08-07 11:01:46 -0600

    fix: unused line

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import { QueryClient } from "@tanstack/react-query";
 import type { Metadata, Viewport } from "next";
 import { Manrope } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 import { Providers } from "./providers";
-import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Test Web Frontend",
@@ -22,8 +21,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
-
   return (
     <ClerkProvider>
       <html
